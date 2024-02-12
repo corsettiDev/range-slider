57661c3c568426dda97a01d7e3538bd9bbf461b4
corsettiDev <patrick@corsetti.dev>
2024-02-12 12:41:15 -0500

    Optimized the slider update into its own function
    In the forEach method on the sliders, I've simplified updating the
    styling of the slider into a function caleld updateSlider which can be
    applied to the range or number inputs.

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ cssLink.href = "styles.css";
 
 document.head.appendChild(cssLink);
 
-const sliderEl = document.querySelector("#range");
-const sliderValue = document.querySelector("#range-number");
+// const sliderEl = document.querySelector("#range");
+// const sliderValue = document.querySelector("#range-number");
 
 const sliders = document.querySelectorAll("input[type=range]");
 
@@ -13,33 +13,23 @@ const sliders = document.querySelectorAll("input[type=range]");
 (function () {
   sliders.forEach((slider) => {
     const sliderValue = slider.nextElementSibling;
-    sliderValue.addEventListener("input", (event) => {
-      let tempSliderValue;
-      if (!sliderValue.value) {
-        tempSliderValue = 0;
-      } else if (sliderValue.value > sliderValue.max) {
-        tempSliderValue = sliderValue.max;
-        sliderValue.value = sliderValue.max;
-      } else {
-        tempSliderValue = event.target.value;
-      }
-      slider.value = tempSliderValue;
 
+    const updateSlider = (value) => {
+      const tempSliderValue = value > slider.max ? slider.max : value || 0;
+      slider.value = tempSliderValue;
+      sliderValue.value = tempSliderValue;
       const progress = (tempSliderValue / slider.max) * 100;
       slider.style.background = `linear-gradient(to right, var(--track) ${progress}%, var(--track-bg) ${progress}%)`;
-    });
+    };
 
-    const progress = (slider.value / slider.max) * 100;
-    slider.style.background = `linear-gradient(to right, var(--track) ${progress}%, var(--track-bg) ${progress}%)`;
+    sliderValue.addEventListener("input", (event) => {
+      updateSlider(parseFloat(event.target.value));
+    });
 
     slider.addEventListener("input", (event) => {
-      const tempSliderValue = event.target.value;
-
-      sliderValue.value = tempSliderValue;
-
-      const progress = (tempSliderValue / slider.max) * 100;
-
-      slider.style.background = `linear-gradient(to right, var(--track) ${progress}%, var(--track-bg) ${progress}%)`;
+      updateSlider(parseFloat(event.target.value));
     });
+
+    updateSlider(parseFloat(slider.value));
   });
 })();
